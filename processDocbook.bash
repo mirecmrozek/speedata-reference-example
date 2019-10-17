9c94088dcceffd1aa51e52b490269241850f5bbd
mirecmrozek <miroslav.mrozek@vacuumlabs.com>
2019-10-17 16:25:45 +0200

    use VL contract as sample input

--- processDocbook.bash
+++ processDocbook.bash
@@ -1,13 +1,15 @@
 # create source xml
-asciidoctor cheatsheet.adoc -b docbook
+asciidoctor data.adoc -b docbook -o data.xml
 
 # replace <emphasis> with <b> and <i> tags
-perl -0777 -i -pe "s/\<emphasis role=\"strong\"\>([^\<\>]*)\<\/emphasis\>/\&lt;b\&gt;\1\&lt;\/b\&gt;/gm" cheatsheet.xml
-perl -0777 -i -pe "s/\<emphasis\>([^\<\>]*)\<\/emphasis\>/\&lt;i\&gt;\1\&lt;\/i\&gt;/gm" cheatsheet.xml
-perl -0777 -i -pe "s/\<emphasis role=\"strong\"\>([^\<\>]*)\<\/emphasis\>/\&lt;b\&gt;\1\&lt;\/b\&gt;/gm" cheatsheet.xml
+perl -0777 -i -pe "s/\<emphasis role=\"strong\"\>([^\<\>]*)\<\/emphasis\>/\&lt;b\&gt;\1\&lt;\/b\&gt;/gm" data.xml
+perl -0777 -i -pe "s/\<emphasis\>([^\<\>]*)\<\/emphasis\>/\&lt;i\&gt;\1\&lt;\/i\&gt;/gm" data.xml
+perl -0777 -i -pe "s/\<emphasis role=\"strong\"\>([^\<\>]*)\<\/emphasis\>/\&lt;b\&gt;\1\&lt;\/b\&gt;/gm" data.xml
 
+# delete <?> tags which confuse speedata
+sed '/^<?.*?>$/d' -i data.xml
 # replace reference tags with our own syntax
-sed '/^<?.*?>$/d' cheatsheet.xml | sed -r 's/<xref linkend="([a-zA-Z0-9]*)"\/>/__\1__/g' > data.xml
+sed data.xml -ri's/<xref linkend="([a-zA-Z0-9]*)"\/>/__\1__/g'
 
 # include files
 sp --prepend-xml=fonts.xml --prepend-xml=pagination.xml
